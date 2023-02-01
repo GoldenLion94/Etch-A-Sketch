855f98121d82e6e5bee9f1b7b1142cc7adacc8a6
Milton Sanchez <miltonsanchez5081@gmail.com>
2023-02-01 14:01:23 -0500

    add initial js for grid div manipulation

---
+++ index.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let container = document.querySelector(".container");
+
+  container.style.gridTemplateColumns = "repeat(16, 1fr)";
+  container.style.gridTemplateRows = "repeat(16, 1fr)";
+});
