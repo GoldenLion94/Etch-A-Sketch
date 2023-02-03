8ef648ea3ca1607853573e913df2998f4b816dc2
Milton Sanchez <miltonsanchez5081@gmail.com>
2023-02-03 12:08:53 -0500

    create board function

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
+  createBoard(32);
+});
+
+function createBoard(size) {
   let container = document.querySelector(".container");
 
-  container.style.gridTemplateColumns = "repeat(16, 1fr)";
-  container.style.gridTemplateRows = "repeat(16, 1fr)";
-});
+  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+  let numDivs = size * size;
+
+  for (let i = 0; i < numDivs; i++) {
+    let div = document.createElement("div");
+    div.style.backgroundColor = "red";
+    container.insertAdjacentElement("beforeend", div);
+  }
+}
